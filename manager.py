8df73a5cf2d090c53688d59da9be90f72fe3a95e
auhjin <auhjin_ai8@163.com>
2021-06-16 11:41:01 +0800

    配置Session信息

--- manager.py
+++ manager.py
@@ -4,7 +4,10 @@
 @file:manager.py
 @time:2021/06/16
 """
-from flask import Flask
+from datetime import timedelta
+
+from flask import Flask,session
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 
@@ -21,13 +24,25 @@ class Config(object):
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
 
+    #Session配置信息
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = timedelta(day = 2)
+
 app.config.from_object(Config)
 
 #创建数据库对象
 db = SQLAlchemy(app)
 redis_store = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT,decode_responses=True)
 
+Session(app)
+
 def hellp_world():
+
+    session['name'] = "auhjin"
+    print(session.get("name"))
+
     return "hello world!"
 
 if __name__ == "__main__":
