4b255deb614a85e6b86cdc8feda860c9dabf6cae
auhjin <auhjin_ai8@163.com>
2021-06-28 23:34:48 +0800

    CSRFProtect校验开启

--- info/__init__.py
+++ info/__init__.py
@@ -11,6 +11,8 @@ from flask import Flask
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
+from flask_wtf.csrf import generate_csrf
+
 from redis import StrictRedis
 
 from config import envs, Config
@@ -40,7 +42,7 @@ def creat_app(config_name):
 
     Session(app)
 
-    # CSRFProtect(app)
+    CSRFProtect(app)
 
     from info.modules.index import index_blue
     app.register_blueprint(index_blue)
@@ -48,8 +50,17 @@ def creat_app(config_name):
     from info.modules.passport import passport_blue
     app.register_blueprint(passport_blue)
 
-    # print(app.url_map)
+    #使用请求钩子拦截所有的请求，通过在cookie中设置csrf_token
+    @app.after_request
+    def after_request(resp):
+        #调用系统方法，获取csrf_token
+        csrf_token = generate_csrf()
+        #将csrf_token设置到cookie中
+        resp.set_cookie("csrf_token",csrf_token)
+        # 返回响应
+        return resp
 
+    # print(app.url_map)
     return app
 
 def log_file(level_name):
