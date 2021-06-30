a466a45beca9ffc855900e5e528e42af1bd412ba
auhjin <auhjin_ai8@163.com>
2021-06-30 23:59:18 +0800

    新闻详情页面展示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -4,9 +4,32 @@
 @file:views.py
 @time:2021/06/30
 """
+from flask import current_app, jsonify, render_template, abort
+
 from . import  news_blue
+from ...models import News
 
-@news_blue.route("/news_detail")
-def news_detail():
 
-    return "展示新闻详细内容"
+# 新闻详情请求接口
+# 请求路径： /news/<int:news_id>
+# 请求方式： get
+# 请求参数： news_id
+# 返回数据： detail.html页面 data字典数据
+from ...utils.response_code import RET
+
+
+@news_blue.route("/<int:news_id>")
+def news_detail(news_id):
+    # 1、根据新闻编号，查询新闻对象
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="获取新闻信息失败")
+    if not news:
+        abort(404)
+    # 2、携带数据，渲染界面
+    data = {
+        "news_info":news.to_dict() if news else ""
+    }
+    return render_template("news/detail.html", data=data)
